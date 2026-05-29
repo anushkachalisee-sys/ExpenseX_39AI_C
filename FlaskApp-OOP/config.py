725cabff7df533f6984bcf381957f3147c04ca22
anushkachalisee-sys <anushkachalisee@gmail.com>
2026-05-29 21:38:51 +0545

    fixed errors

--- FlaskApp-OOP/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
-MYSQL_HOST = os.environ.get("MYSQL_HOST")
-MYSQL_USER = os.environ.get("MYSQL_USER")
-MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
-MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
