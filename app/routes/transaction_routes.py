d8db787c571e040e9cd579dbc47c67ea8adc83f0
prayash <168839495+prayashthagunna@users.noreply.github.com>
2026-06-19 23:29:32 +0545

    updated

--- app/routes/transaction_routes.py
+++ app/routes/transaction_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+~from flask import Blueprint
 #Check routes
 #working in this file
 from app.auth import user_required
@@ -40,4 +40,4 @@ class TransactionRoutes:
     @user_required
     def delete(self, txn_id):
         return self.controller.delete(txn_id)
-# verification by kushumlata
+# verification by kushumlata~
